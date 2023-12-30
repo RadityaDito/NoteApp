67694e3b4c79d99fa3c772614a84283d47bc359e
RadityaDito <raditya.ihsan@ui.ac.id>
2023-12-31 03:34:35 +0700

    Update package.json script

--- providers/ChakraUIProvider.tsx
+++ providers/ChakraUIProvider.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
+//Chakra UI cant be used with ShadcnUI
 export function ChakraUIProvider({ children }: { children: ReactNode }) {
   return <ChakraProvider>{children}</ChakraProvider>;
 }
